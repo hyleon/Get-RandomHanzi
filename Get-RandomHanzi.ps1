aa35bf8af0e1b6a4b6b900f7e0ca4411fa610067
hyleon <38688258+hyleon@users.noreply.github.com>
2020-11-27 10:51:34 +0800

    Add files via upload

---
+++ Get-RandomHanzi.ps1
@@ -0,0 +1,19 @@
+﻿Function Get-RandomHanzi
+{
+    
+    Param (
+        [int]$Length = 1
+    )
+    if ($Length -le 0) {
+        return
+    }
+    $count = 1
+    do {
+        $Hanzihex = '{0:x}' -f $(Get-Random -Minimum 0x4e00 -Maximum 0x9fa5)
+        $Hanzibyte = ($Hanzihex -split '(\w{2})'|?{$_}|%{[int16]"0x$_"})[1,0]
+        $Hanzi += [System.Text.Encoding]::GetEncoding('Unicode').GetString($Hanzibyte)
+        $count += 1
+    }
+    while ($count -le $Length)
+    return $Hanzi
+}
